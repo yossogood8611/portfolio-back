04e8286603807183cdb46e72904d5a40bfa8a8b0
DESKTOP-KPD2CO1\roopr <rooproop1111@naver.com>
2021-03-05 18:23:50 +0900

    no message

--- routes/techstack.js
+++ routes/techstack.js
@@ -5,16 +5,15 @@ const pool = require('../pool')
 /**
  * @swagger
  * tags:
- *   name: techstack
+ *   name: tech-stack
  *   description: 기술스택
  */
 /**
  * @swagger
- * /tech :
+ * /tech:
  *   get:
- *     summary: 기술 스택 정보 가져오기
- *     tags: [techstack]
- *
+ *     summary: 기슬 스택 정보 가져오기
+ *     tags: [tech-stack]
  *     responses:
  *       200:
  *         description: 성공
@@ -26,12 +25,52 @@ const pool = require('../pool')
  *         $ref: '#/components/res/BadRequest'
  */
 router.get('/', async (req,res,next) => {
-        try{
-            const data= await pool.query('select * from techstack')
-            return res.json(data[0])
-        }catch (err){
-            return  res.status(400).json(err)
-        }
+    try{
+        const data = await pool.query('select * from techstack')
+        return res.json(data[0])
+    }catch (err){
+        return  res.status(400).json(err)
+    }
+})
+/**
+ * @swagger
+ * /tech/pro:
+ *   post:
+ *     summary: 기슬 스택 정보 입력하기
+ *     tags: [tech-stack]
+ *     consumes:
+ *       - application/x-www-form-urlencoded
+ *     requestBody:
+ *       content:
+ *          application/x-www-form-urlencoded:
+ *              schema:
+ *                  type: object
+ *                  properties:
+ *                      name:
+ *                          type: string
+ *                      stack:
+ *                          type: int
+ *              required:
+ *                  - name
+ *                  - stack
+ *     responses:
+ *       200:
+ *         description: 성공
+ *       403:
+ *         $ref: '#/components/res/Forbidden'
+ *       404:
+ *         $ref: '#/components/res/NotFound'
+ *       400:
+ *         $ref: '#/components/res/BadRequest'
+ */
+router.post('/pro', async (req,res,next) => {
+    try{
+        let {name, stack}=req.body;
+        const data = await pool.query('INSERT INTO techstack (name, stack) values(?,?)',[name,stack])
+        return res.json(data[0])
+    }catch (err){
+        return  res.status(400).json(err)
+    }
 })
 
 module.exports = router;
