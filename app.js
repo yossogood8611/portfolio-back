52e0663cfa6128ebc0e0c8abe3e7de53a542f34f
DESKTOP-KPD2CO1\roopr <rooproop1111@naver.com>
2021-03-16 00:06:45 +0900

    feat : add cors module

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 const asyncify = require('express-asyncify');
 const router = express.Router();
 
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 app.use('/', router);
 app.use('/about', require('./routes/about'));
 app.use('/pw', require('./routes/pw'));
